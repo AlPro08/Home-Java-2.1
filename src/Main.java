0425f22696410e46b78816ba223575bcbdca7659
Aleksandr Prokhorov <proxor1186@gmail.com>
2022-06-15 21:41:39 +0300

    raschet bonusov

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double ticketCost = 5754.45;
+        int step = 20;
+        int bonus = (int) ticketCost / step;
+
+        System.out.println("количество бонусных рублей = " + bonus);
+    }
+}
