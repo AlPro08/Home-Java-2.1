778f4ca9b9dd84b5a44f389adefb116aa87e5828
Aleksandr Prokhorov <proxor1186@gmail.com>
2022-06-15 21:48:14 +0300

    raschet bonusov v2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        double ticketCost = 5754.45;
-        int step = 20;
+        double ticketCost = 5754.45; // стоимость билета
+        int step = 20; // количество рублей для одной бонусной мили
         int bonus = (int) ticketCost / step;
 
         System.out.println("количество бонусных рублей = " + bonus);
